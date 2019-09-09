0afca3e3180b969a6eb5a30bc4e35255f01a519d
alffox <alfonso.crisci.it@gmail.com>
2019-09-09 10:49:20 +0200

    feat: Implement Clay Lexicon SVG icon component

--- src/main/resources/META-INF/resources/lib/modules/AtwTimeDate.es.js
+++ src/main/resources/META-INF/resources/lib/modules/AtwTimeDate.es.js
@@ -1,13 +1,15 @@
 import React from "react";
 
+import AtwIcon from "./AtwIcon.es";
+
 class AtwTimeDate extends React.Component {
   render() {
     return (
       <div className="col-md-12 text-info font-weight-bold text-center">
         <div className="timedate">
-          <i className="icon-calendar" />
+          <AtwIcon classes="lexicon-icon inline-item" iconName="calendar" />
           <span className="date px-1 mr-3">{this.props.date}</span>
-          <i className="icon-time" />
+          <AtwIcon classes="lexicon-icon inline-item" iconName="time" />
           <span className="time px-1">{this.props.time}</span>
         </div>
       </div>
