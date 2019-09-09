43642c9990c3c18ddd0515f7afcaa501aa66f8c7
alffox <alfonso.crisci.it@gmail.com>
2019-09-09 09:55:32 +0200

    feat: Make whole new an anchor

--- src/main/resources/META-INF/resources/lib/modules/AtwNew.es.js
+++ src/main/resources/META-INF/resources/lib/modules/AtwNew.es.js
@@ -6,26 +6,26 @@ class AtwNew extends React.Component {
   render() {
     return (
       <ul className="list-group">
-        <li className="list-group-item list-group-item-action active d-flex justify-content-between align-items-center mt-1">
-          <LazyLoad>
-            <img
-              className="img-fluid img-thumbnail news-picture"
-              alt={this.props.articleTitle}
-              src={this.props.imageURL}
-            />
-          </LazyLoad>
-          <a
-            href={this.props.articleURL}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="list-group-item list-group-item-action active"
-          >
-            {this.props.articleTitle}
-          </a>
-          <span className="badge badge-light badge-primary d-none d-xl-block">
-            {this.props.articleSource}
-          </span>
-        </li>
+        <a
+          href={this.props.articleURL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="list-group-item list-group-item-action active"
+        >
+          <li className="list-group-item list-group-item-action active d-flex justify-content-between align-items-center mt-1">
+            <LazyLoad>
+              <img
+                className="img-fluid img-thumbnail news-picture"
+                alt={this.props.articleTitle}
+                src={this.props.imageURL}
+              />
+            </LazyLoad>
+            <h3 className="ml-2">{this.props.articleTitle}</h3>
+            <span className="badge badge-light badge-primary d-none d-xl-block">
+              {this.props.articleSource}
+            </span>
+          </li>
+        </a>
       </ul>
     );
   }
