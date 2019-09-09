da3cd2e38b6ea3231490229ef8cff570921b5195
alffox <alfonso.crisci.it@gmail.com>
2019-09-09 08:15:14 +0200

    bug: Add more spacing in date-time

--- src/main/resources/META-INF/resources/lib/modules/AtwTimeDate.es.js
+++ src/main/resources/META-INF/resources/lib/modules/AtwTimeDate.es.js
@@ -3,10 +3,10 @@ import React from "react";
 class AtwTimeDate extends React.Component {
   render() {
     return (
-      <div className="col-md-12 text-info font-weight-bold text-center font-italic">
+      <div className="col-md-12 text-info font-weight-bold text-center">
         <div className="timedate">
           <i className="icon-calendar" />
-          <span className="date px-1">{this.props.date}</span>
+          <span className="date px-1 mr-3">{this.props.date}</span>
           <i className="icon-time" />
           <span className="time px-1">{this.props.time}</span>
         </div>
